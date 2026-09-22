c04ffc46ec1fe3ea7578d8ef2bb67eef5e0ebe35
jrtknwp <jrtknwp@gmail.com>
2026-09-22 23:25:48 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = "my-account-v8-ocr-thai";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./ocr/tesseract.min.js", "./ocr/worker.min.js", "./ocr/core/tesseract-core-simd-lstm.wasm.js", "./ocr/lang/tha.traineddata"];
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", (event) => event.waitUntil(Promise.all([
+  caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))),
+  self.clients.claim()
+])));
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
